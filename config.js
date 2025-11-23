06aa08062f666ba1ec8befe8dd122e65b449cc53
Shehzad Ahmed <ffshehzadffff@outlook.com>
2025-11-23 16:08:57 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1442087644262826015/J5_25sj22eKocc-i_0SrMpOx5tAu3lwRmJGzaDA58SM3RYDArzOSCq5dO64GteLRRp1P",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
